1a8b27f395e444796cf6434e74ba59ad3bb9b51d
Andy Dang <andyhd0614@gmail.com>
2026-05-31 09:17:59 -0700

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navigation from "./components/Navigation";
+import Navigation from "./components/navigation/Navigation";
 import Home from "./pages/home/Home";
 import StyleGuide from "./pages/style-guide/StyleGuide";
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <Navigation />
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/style-guide" element={<StyleGuide />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/style-guide' element={<StyleGuide />} />
           </Routes>
         </main>
       </div>
